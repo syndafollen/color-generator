3a8fb25412ba2ea376a31738ced4060297e8841b
kgfk kjglk <kinou@LAPTOP-JUFQ2MS0.agriwell.com>
2023-02-25 10:53:38 +0100

    Changes

--- app.js
+++ app.js
@@ -1,96 +1,109 @@
-const cols = document.querySelectorAll('.col');
+import { generateToast} from "./toast/toast.js";
 
-document.addEventListener("click", (event) => {
-    const type = event.target.dataset.type
+const cols = document.querySelectorAll(".col");
+const colorCodes = document.querySelectorAll('h2[data-type="copy"]');
+const locks = document.querySelectorAll('button[data-type="lock"]');
 
-    if(type === "lock") {
-        const node = event.target.tagName.toLowerCase() === "i" ? event.target : event.target.children[0]
-        node.classList.toggle("fa-lock-open");
-        node.classList.toggle("fa-lock")
-    } else if(type === "copy") {
-        copyColorToClick(event.target.textContent);
+colorCodes.forEach((elem) => {
+  elem.addEventListener("click", (event) => {
+    copyColorToClick(event.target.textContent);
+    generateToast({ columnId: event.target.id, message: "Copied" });
+  });
+});
+
+locks.forEach((elem) => {
+  elem.addEventListener("click", (event) => {
+    const type = event.target.dataset.type;
+    if (type === "lock") {
+      const node =
+        event.target.tagName.toLowerCase() === "i"
+          ? event.target
+          : event.target.children[0];
+      node.classList.toggle("fa-lock-open");
+      node.classList.toggle("fa-lock");
+    } else if (type === "copy") {
+      // copyColorToClick(event.target.textContent);
+      generateToast({ message: "fff" });
     }
-})
+  });
+});
 
 //obnovení barev při zmačknutí spacebaru
-document.addEventListener('keydown', (event) => {
-    event.preventDefault();
-   if(event.code.toLowerCase() === "space") {
-    setRandomColors()
-   }
-}
-)
-
-
-//nastavovani toastu
-let toastContainer;
-const toastElement = document.querySelector("#toast");
-const generateToast = 
-
+document.addEventListener("keydown", (event) => {
+  event.preventDefault();
+  if (event.code.toLowerCase() === "space") {
+    setRandomColors();
+  }
+});
 
 //kopírování kódu barvy
 function copyColorToClick(text) {
-    return navigator.clipboard.writeText(text);
+  return navigator.clipboard.writeText(text);
 }
 
 function generateRandomColor() {
-    const hexCode = "0123456789ABCDEF";
-    let color = "";
-    for(let i = 0; i < 6; i++) {
-        color += hexCode[Math.floor(Math.random() * hexCode.length)];
-    }
-    return "#" + color;
+  const hexCode = "0123456789ABCDEF";
+  let color = "";
+  for (let i = 0; i < 6; i++) {
+    color += hexCode[Math.floor(Math.random() * hexCode.length)];
+  }
+  return "#" + color;
 }
 
 //obnovení barev
-function setRandomColors(isInitial) { 
-    const colors = isInitial ? getColorsFromHash() : [];
+function setRandomColors(isInitial) {
+  const colors = isInitial ? getColorsFromHash() : [];
 
-    cols.forEach((col, index) => {
-        const isLocked = col.querySelector("i").classList.contains("fa-lock");
-        const text = col.querySelector("h2");
-        const button = col.querySelector("button");
+  cols.forEach((col, index) => {
+    const isLocked = col.querySelector("i").classList.contains("fa-lock");
+    const text = col.querySelector("h2");
+    const button = col.querySelector("button");
 
-        if(isLocked) {
-        colors.push(text.textContent);
-        return
-         }
+    if (isLocked) {
+      colors.push(text.textContent);
+      return;
+    }
 
-        const color = isInitial 
+    const color = isInitial
+      ? colors[index]
         ? colors[index]
-            ? colors[index] 
-         : generateRandomColor()
-        : generateRandomColor();
+        : generateRandomColor()
+      : generateRandomColor();
 
-        if(!isInitial) {
-        colors.push(color);
-        }
+    if (!isInitial) {
+      colors.push(color);
+    }
 
-        text.textContent = color;
-        col.style.background = color;
-        setTextColor(text, color)
-        setTextColor(button, color)
-        })
+    text.textContent = color;
+    col.style.background = color;
+    setTextColor(text, color);
+    setTextColor(button, color);
+  });
 
-    updateColorsHash(colors);
+  updateColorsHash(colors);
 }
 
 function setTextColor(text, color) {
-    const luminance = chroma(color).luminance();
-    text.style.color = luminance > 0.5 ? "#34282C" : "#FFFFF0";
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? "#34282C" : "#FFFFF0";
 }
 
 function updateColorsHash(colors = []) {
-    document.location.hash = colors.map((col) => {
-        return col.toString().substring(1)
-}).join("-");
+  document.location.hash = colors
+    .map((col) => {
+      return col.toString().substring(1);
+    })
+    .join("-");
 }
 
 function getColorsFromHash() {
-    if(document.location.hash.length > 1) {
-        return document.location.hash.substring(1).split("-").map(color => "#" + color)
-    }
-    return []
+  if (document.location.hash.length > 1) {
+    return document.location.hash
+      .substring(1)
+      .split("-")
+      .map((color) => "#" + color);
+  }
+  return [];
 }
 
-setRandomColors(true)
+setRandomColors(true);
---
+++ toast/toast.js
@@ -0,0 +1,25 @@
+export const generateToast = ({
+    columnId,
+    message,
+    backgroundColor = "#4682B4",
+    color = "#fffffe",
+    length = "2000ms"
+  }) => {
+    const column = document.querySelector(`div[id=container${columnId}]`);
+    console.log(column);
+  
+    column.insertAdjacentHTML(
+      "afterbegin",
+      `<p class='toast'
+        style="background-color: ${backgroundColor};
+        color: ${color};
+        animation-duration: ${length}">
+        ${message}
+        </p>`
+    );
+    const toast = document.querySelector(".toast");
+    toast.addEventListener("animationend", () => {
+      toast.remove();
+    });
+  };
+  
