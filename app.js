7ad3678addc6911d64059089a129ca2af3f1fbdf
kgfk kjglk <kinou@LAPTOP-JUFQ2MS0.agriwell.com>
2023-03-06 16:13:37 +0100

    Changes app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-import { generateToast} from "./toast/toast.js";
+import { generateToast } from "./toast/toast.js";
+import chroma from "chroma-js"
 
 const cols = document.querySelectorAll(".col");
 const colorCodes = document.querySelectorAll('h2[data-type="copy"]');
 const locks = document.querySelectorAll('button[data-type="lock"]');
+let colors = []
 
 colorCodes.forEach((elem) => {
   elem.addEventListener("click", (event) => {
@@ -11,20 +13,18 @@ colorCodes.forEach((elem) => {
   });
 });
 
-locks.forEach((elem) => {
-  elem.addEventListener("click", (event) => {
-    const type = event.target.dataset.type;
-    if (type === "lock") {
-      const node =
-        event.target.tagName.toLowerCase() === "i"
-          ? event.target
-          : event.target.children[0];
-      node.classList.toggle("fa-lock-open");
-      node.classList.toggle("fa-lock");
-    } else if (type === "copy") {
-      // copyColorToClick(event.target.textContent);
-      generateToast({ message: "fff" });
+locks.forEach((elem, index) => {
+  elem.addEventListener("click", (event) => {  
+    const colorCode = event.currentTarget.parentNode.innerText
+    const colorsArray  = colors.filter(({ color }) => color === colorCode)
+
+    if (colorsArray.length === 0) {
+      colors.push({ num: index, color: colorCode })
+    } else {
+      colors = colors.filter(({ color }) => color !== colorCode)
     }
+
+    updateColorsHash(colors)
   });
 });
 
@@ -37,11 +37,11 @@ document.addEventListener("keydown", (event) => {
 });
 
 //kopírování kódu barvy
-function copyColorToClick(text) {
+const copyColorToClick = (text) => {
   return navigator.clipboard.writeText(text);
 }
 
-function generateRandomColor() {
+const generateRandomColor = () => {
   const hexCode = "0123456789ABCDEF";
   let color = "";
   for (let i = 0; i < 6; i++) {
@@ -51,27 +51,26 @@ function generateRandomColor() {
 }
 
 //obnovení barev
-function setRandomColors(isInitial) {
-  const colors = isInitial ? getColorsFromHash() : [];
+const setRandomColors = () => {
+  const isInitial = !!colors.length
 
   cols.forEach((col, index) => {
     const isLocked = col.querySelector("i").classList.contains("fa-lock");
     const text = col.querySelector("h2");
     const button = col.querySelector("button");
 
+    const colColors = colors.filter(({ num }) => index === num)
+    let color = '';
+
+    if (!!colColors.length) {
+      color = colColors[0].color
+    } else {
+      color = generateRandomColor();
+    }
+
     if (isLocked) {
       colors.push(text.textContent);
       return;
-    }
-
-    const color = isInitial
-      ? colors[index]
-        ? colors[index]
-        : generateRandomColor()
-      : generateRandomColor();
-
-    if (!isInitial) {
-      colors.push(color);
     }
 
     text.textContent = color;
@@ -79,31 +78,66 @@ function setRandomColors(isInitial) {
     setTextColor(text, color);
     setTextColor(button, color);
   });
-
-  updateColorsHash(colors);
 }
 
-function setTextColor(text, color) {
+
+const setTextColor = (text, color) => {
   const luminance = chroma(color).luminance();
   text.style.color = luminance > 0.5 ? "#34282C" : "#FFFFF0";
 }
 
-function updateColorsHash(colors = []) {
-  document.location.hash = colors
+
+const updateColorsHash = (colors = []) => {
+  const onlyColors = colors.map(elem => elem.color)
+
+  window.location.hash = onlyColors
     .map((col) => {
       return col.toString().substring(1);
     })
-    .join("-");
+    .join(`-${index}#`); // ????
+
+  locks.forEach((elem) => {
+    const node = elem.children[0]
+  
+    if (onlyColors.includes(elem.parentNode.firstElementChild.innerText)) {
+        node.classList.add("fa-lock")
+        node.classList.remove("fa-lock-open")
+    } else {
+      node.classList.add("fa-lock-open")
+        node.classList.remove("fa-lock")
+    }
+  })
+  
 }
 
-function getColorsFromHash() {
-  if (document.location.hash.length > 1) {
-    return document.location.hash
-      .substring(1)
-      .split("-")
-      .map((color) => "#" + color);
+const getColorsFromHash = () => {
+  if (window.location.hash.length > 1) {
+    const uniqueValues = [...new Set(window.location.hash.split('-'))]
+    
+    window.location.hash = uniqueValues.join('-')
+
+    console.log('aaa:', uniqueValues.map((color, index) => ({ num: index, color}))) 
+
+    return uniqueValues.map((color, index) => ({ num: index, color})) // http://localhost:1234/#0#4FC800-1#A2A0D1-2#FFFFFF
   }
+
   return [];
 }
 
-setRandomColors(true);
+(() => {
+  colors = getColorsFromHash()
+  setRandomColors();
+
+  locks.forEach((elem)=>{
+    const node = elem.children[0]
+    const colorsArray  = colors.filter(({ color }) => color === elem.parentNode.firstElementChild.innerText) 
+  
+    if (!!colorsArray.length) {
+        node.classList.add("fa-lock")
+        node.classList.remove("fa-lock-open")
+    } else {
+      node.classList.add("fa-lock-open")
+      node.classList.remove("fa-lock")
+    }
+  })
+})()
